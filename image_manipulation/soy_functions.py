0b40b25e45f14dfa8f1a91769d04c41af39c994b
FuchsMann <kommandantfuchs@protonmail.com>
2023-05-16 09:07:33 +0200

    pathing fixes

--- image_manipulation/soy_functions.py
+++ image_manipulation/soy_functions.py
@@ -8,37 +8,37 @@ import requests
 class SoyFunctions:
     def soy(imageUrl: str) -> File:
         inImage = Image.open(BytesIO(requests.get(imageUrl).content))
-        overImage = Image.open(Path("images", "soy.png"))
+        overImage = Image.open(Path('image_manipulation', 'images', 'soyjaks.png'))
         overImage = overImage.resize(inImage.size, Image.ANTIALIAS)
         inImage.paste(
             overImage, (0, 0, overImage.size[0], overImage.size[1]), overImage
         )
         byteArr = BytesIO()
-        inImage.save(byteArr, "soy.jpg")
+        inImage.save(byteArr, 'Soy.jpg')
         byteArr.seek(0)
-        return File(byteArr, filename="Soy.jpg")
+        return File(byteArr, filename='Soy.jpg')
 
     def soyphone(imageUrl: str) -> File:
         inImage = Image.open(
             BytesIO(requests.get(imageUrl).content)
-        ).convert("RGBA")
+        ).convert('RGBA')
         if inImage.size[0] / inImage.size[1] >= 1:
-            overImage = Image.open(Path("images", "soyphoneH.png"))
-            rootImage = Image.new("RGBA", overImage.size, (255, 255, 255))
+            overImage = Image.open(Path('image_manipulation', 'images', 'soyphoneH.png'))
+            rootImage = Image.new('RGBA', overImage.size, (255, 255, 255))
             inImage = inImage.rotate(-0.92, expand=1)
             inImage = inImage.resize((372, 216), Image.ANTIALIAS)
             rootImage.paste(inImage, (71, 225, 443, 441), inImage)
         else:
-            overImage = Image.open(Path("images", "soyphoneV.png"))
-            rootImage = Image.new("RGBA", overImage.size, (255, 255, 255))
+            overImage = Image.open(Path('image_manipulation', 'images', 'soyphoneV.png'))
+            rootImage = Image.new('RGBA', overImage.size, (255, 255, 255))
             inImage = inImage.rotate(4.78, expand=1)
             inImage = inImage.resize((276, 447), Image.ANTIALIAS)
             rootImage.paste(inImage, (24, 65, 300, 512), inImage)
         rootImage.paste(
             overImage, (0, 0, overImage.size[0], overImage.size[1]), overImage
         )
-        rootImage = rootImage.convert("RGB")
+        rootImage = rootImage.convert('RGB')
         byteArr = BytesIO()
         rootImage.save(byteArr, 'Soyphone.jpg')
         byteArr.seek(0)
-        return File(byteArr, filename="Soyphone.jpg")
+        return File(byteArr, filename='Soyphone.jpg')

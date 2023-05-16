8fafccc0227a048c52a9b3f29e4a4b0fe2ec6418
FuchsMann <kommandantfuchs@protonmail.com>
2023-05-16 09:38:32 +0200

    jpg error fix

--- image_manipulation/soy_functions.py
+++ image_manipulation/soy_functions.py
@@ -7,7 +7,7 @@ import requests
 
 class SoyFunctions:
     def soy(imageUrl: str) -> File:
-        inImage = Image.open(BytesIO(requests.get(imageUrl).content))
+        inImage = Image.open(BytesIO(requests.get(imageUrl).content)).convert('RGBA')
         overImage = Image.open(Path('image_manipulation', 'images', 'soyjaks.png'))
         overImage = overImage.resize(inImage.size, Image.ANTIALIAS)
         inImage.paste(
@@ -19,17 +19,19 @@ class SoyFunctions:
         return File(byteArr, filename='Soy.jpg')
 
     def soyphone(imageUrl: str) -> File:
-        inImage = Image.open(
-            BytesIO(requests.get(imageUrl).content)
-        ).convert('RGBA')
+        inImage = Image.open(BytesIO(requests.get(imageUrl).content)).convert('RGBA')
         if inImage.size[0] / inImage.size[1] >= 1:
-            overImage = Image.open(Path('image_manipulation', 'images', 'soyphoneH.png'))
+            overImage = Image.open(
+                Path('image_manipulation', 'images', 'soyphoneH.png')
+            )
             rootImage = Image.new('RGBA', overImage.size, (255, 255, 255))
             inImage = inImage.rotate(-0.92, expand=1)
             inImage = inImage.resize((372, 216), Image.ANTIALIAS)
             rootImage.paste(inImage, (71, 225, 443, 441), inImage)
         else:
-            overImage = Image.open(Path('image_manipulation', 'images', 'soyphoneV.png'))
+            overImage = Image.open(
+                Path('image_manipulation', 'images', 'soyphoneV.png')
+            )
             rootImage = Image.new('RGBA', overImage.size, (255, 255, 255))
             inImage = inImage.rotate(4.78, expand=1)
             inImage = inImage.resize((276, 447), Image.ANTIALIAS)

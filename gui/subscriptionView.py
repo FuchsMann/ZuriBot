82af6d14ee8798e46e1a6fbe188557a4eb8e61c6
FuchsMann <kommandantfuchs@protonmail.com>
2023-05-22 10:12:23 +0200

    mini fix

--- gui/subscriptionView.py
+++ gui/subscriptionView.py
@@ -1,4 +1,4 @@
-from discord.ui import View, UserSelect, ChannelSelect, Button
+from discord.ui import View, UserSelect, ChannelSelect, Button, button
 from discord import Interaction, ButtonStyle
 
 class SubscriptionView(View, title='Subscriptions'):
@@ -7,13 +7,13 @@ class SubscriptionView(View, title='Subscriptions'):
     
     userSelect = UserSelect(placeholder='Select a user', min_values=0, max_values=25, row=1)
     
-    @Button(label='Confirm', style=ButtonStyle.green, row=2)
+    @button(label='Confirm', style=ButtonStyle.green, row=2)
     async def confirm(self, interaction: Interaction, button: Button):
         await interaction.response.send_message('Confirmed.', ephemeral=True)
         self.value = True
         self.stop()
         
-    @Button(label='Cancel', style=ButtonStyle.red, row=2)
+    @button(label='Cancel', style=ButtonStyle.red, row=2)
     async def confirm(self, interaction: Interaction, button: Button):
         await interaction.response.send_message('Aborted.', ephemeral=True)
         self.value = True

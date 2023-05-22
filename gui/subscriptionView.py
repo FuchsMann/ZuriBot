0fac14cc24306603d07c48ef2d8db6c2f5cb2b4e
FuchsMann <kommandantfuchs@protonmail.com>
2023-05-22 11:31:25 +0200

    adv nx

--- gui/subscriptionView.py
+++ gui/subscriptionView.py
@@ -6,7 +6,7 @@ class SubscriptionView(View):
         super().__init__()
         self.value = False
         
-        self.channelSelectHandle = ChannelSelect(placeholder='Select a channel', min_values=0, max_values=25, row=0, channel_types=[ChannelType.voice])
+        self.channelSelectHandle = ChannelSelect(placeholder='Select a channel', min_values=0, max_values=1, row=0, channel_types=[ChannelType.voice])
         self.userSelectHandle = UserSelect(placeholder='Select a user', min_values=0, max_values=25, row=1)
         
         for item in [self.channelSelectHandle, self.userSelectHandle]:
@@ -18,7 +18,7 @@ class SubscriptionView(View):
     
     @button(label='Confirm', style=ButtonStyle.green, row=2)
     async def confirm(self, interaction: Interaction, button: Button):
-        await interaction.response.send_message(f'{self.userSelectHandle.values}; {self.channelSelectHandle.values}', ephemeral=True)
+        await interaction.response.send_message(f'{[user.name for user in self.userSelectHandle.values]}; {self.channelSelectHandle.values[0].name}', ephemeral=True)
         self.value = True
         self.stop()
         

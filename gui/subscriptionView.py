8a090da0b437d6369f101ae81f5ae0afe680dc02
FuchsMann <kommandantfuchs@protonmail.com>
2023-05-22 10:20:01 +0200

    adv 1

--- gui/subscriptionView.py
+++ gui/subscriptionView.py
@@ -1,14 +1,17 @@
 from typing import Optional
 from discord.ui import View, UserSelect, ChannelSelect, Button, button
-from discord import Interaction, ButtonStyle
+from discord import Interaction, ButtonStyle, ChannelType
 
 class SubscriptionView(View):
     def __init__(self):
         super().__init__()
         self.value = False
         
-        self.add_item(ChannelSelect(placeholder='Select a channel', min_values=1, max_values=1, row=0))
-        self.add_item(UserSelect(placeholder='Select a user', min_values=0, max_values=25, row=1))
+        self.channelSelectHandle = ChannelSelect(placeholder='Select a channel', min_values=0, max_values=25, row=1, channel_types=[ChannelType.voice])
+        self.userSelectHandle = UserSelect(placeholder='Select a user', min_values=0, max_values=25, row=1)
+        
+        for item in [self.channelSelectHandle, self.userSelectHandle]:
+            self.add_item(item)
     
     @button(label='Confirm', style=ButtonStyle.green, row=2)
     async def confirm(self, interaction: Interaction, button: Button):
@@ -17,7 +20,7 @@ class SubscriptionView(View):
         self.stop()
         
     @button(label='Cancel', style=ButtonStyle.red, row=2)
-    async def confirm(self, interaction: Interaction, button: Button):
+    async def cancel(self, interaction: Interaction, button: Button):
         await interaction.response.send_message('Aborted.', ephemeral=True)
         self.value = True
         self.stop()

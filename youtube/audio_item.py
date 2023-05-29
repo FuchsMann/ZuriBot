d7d48bbf06273aca1fcabef44fbb37f143d523aa
FuchsMann <kommandantfuchs@protonmail.com>
2023-05-29 20:26:47 +0200

    yt init

--- youtube/audio_item.py
+++ youtube/audio_item.py
@@ -1,8 +1,24 @@
+from typing import BinaryIO
 from pytube import YouTube
 from discord import Member
+from sys import getsizeof
+
 
 class AudioItem:
-    def __init__(self, url: str, requester: Member) -> None:
+    def __init__(self, url: str, requester_id: int) -> None:
         self.url = url
-        self.requester = requester
+        self.requester = requester_id
         self.youtubeObject = YouTube(url)
+        self.audioBytes: BinaryIO
+        stream = self.youtubeObject.streams.filter(
+            only_audio=True).first()
+        if stream is not None:
+            stream.stream_to_buffer(self.audioBytes)
+            # print size of self.audioBytes
+            print(getsizeof(self.audioBytes))
+
+    def writeToFile(self, path: str) -> None:
+        with open(path, 'wb') as file:
+            file.write(self.audioBytes)
+
+ai = AudioItem('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 1)
--- youtube/audio_manager.py
+++ youtube/audio_manager.py
@@ -1,2 +1,16 @@
-class YoutubeAudio:
-    pass
+from audio_item import AudioItem
+
+class AudioManager:
+    def __init__(self) -> None:
+        self.audioItemQueue: list[AudioItem] = []
+        self.currentAudioItem: AudioItem | None = None
+
+    def next(self) -> None:
+        if len(self.audioItemQueue) > 0:
+            self.currentAudioItem = self.audioItemQueue.pop(0)
+        else:
+            self.currentAudioItem = None
+
+    def clear(self) -> None:
+        self.audioItemQueue.clear()
+        self.currentAudioItem = None
